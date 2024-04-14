12205e0139068e3096bbc8f937f66eb28fdbe32d
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 10:26:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//Solution # 9
+// Favourite Number: store you favourite number in a variable then, using the variable, create a message that reveals ypur favourite 
+// number. print that message.
+let favouriteNumber = 3;
+console.log(`My favourite number is ${favouriteNumber}`);
+export {};
---
+++ app.ts
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+
+//Solution # 9
+// Favourite Number: store you favourite number in a variable then, using the variable, create a message that reveals ypur favourite 
+// number. print that message.
+
+let favouriteNumber: number = 3;
+console.log(`My favourite number is ${favouriteNumber}`);
